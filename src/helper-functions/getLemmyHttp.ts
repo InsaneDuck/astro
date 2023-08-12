e781425e78fcf1b48dc8d00f3210b8598b91486a
satya <39309626+InsaneDuck@users.noreply.github.com>
2023-08-12 17:06:35 +0100

    updated

--- src/helper-functions/getLemmyHttp.ts
+++ src/helper-functions/getLemmyHttp.ts
@@ -1,15 +1,14 @@
 import { LemmyHttp } from "lemmy-js-client";
 import { Platform } from "react-native";
 
-let client: LemmyHttp | null = null;
 const baseUrl = "https://lemmy.ml/";
-const operatingSystem = Platform.OS;
+const client: LemmyHttp | null = new LemmyHttp(baseUrl);
 
+const operatingSystem = Platform.OS;
+const options = {
+  headers: { "User-Agent": `Astro ${operatingSystem}` },
+  fetchFunction: undefined,
+};
 export const getLemmyHttp = () => {
-  const options = {
-    headers: { "User-Agent": `Astro ${operatingSystem}` },
-    fetchFunction: undefined,
-  };
-  client = new LemmyHttp(baseUrl);
   return client;
 };
